7473546ff0e339a818c12d67bae594ae29a6298f
uni51 <uni51@users.noreply.github.com>
2019-07-24 23:35:47 +0900

    Part1 finished

--- database/seeds/AttendanceSeeder.php
+++ database/seeds/AttendanceSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class AttendanceSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AttendanceSeeder::class);
+        factory(App\Attendance::class, 50)->create();
     }
 }
