7df27832c2f463a9fa1297f6ac2bcf994982daf1
uni51 <uni51@users.noreply.github.com>
2019-07-25 14:23:53 +0900

    Part2 API cretate finished

--- app/Attendance.php
+++ app/Attendance.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon; 
 
 class Attendance extends Model
 {
@@ -14,4 +15,39 @@ class Attendance extends Model
         'flagged_at',
         'updated_at',
       ];
+ 
+    /**
+     * [QueryScope] 通報済みユーザの除外
+     * ignoreFlagged()というクエリースコープを用意することで、SQLのwhere句として
+     * `flagged_at` IS NULLという条件の役割を持たせることが可能になります
+     * 
+     * @param $query
+     * @return mixed
+     */
+    public function scopeIgnoreFlagged($query)
+    {
+        return $query->where('flagged_at', null);
+    }
+    
+    /**
+     * 通報処理（flagged_at の更新）
+     * 
+     * @return bool
+     */
+    public function flag()
+    {
+        return $this->update( [ 'flagged_at' => now() ] );
+    }
+    
+
+    /**
+     * Gravatarからアバター画像を取得するメソッドをModel側に追加しておきます。
+     * 
+     * get[プロパティ名]Attribute()という命名規則でメソッド定義しておくことで、
+     * スキーマには存在しないような独自の値を返却することも可能になります。
+     */    
+    public function getAvatarAttribute()
+    {
+        return sprintf( 'https://www.gravatar.com/avatar/%s?s=100', md5( strtolower( trim( $this->email ) ) ) );
+    }    
 }
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('attendances', 'Api\AttendancesController')
+    ->only(['index', 'show', 'store']);
+ 
+Route::put('attendances/{attendance}/report', 'Api\ReportAttendancesController@update');
